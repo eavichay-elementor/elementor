a3f755f67bd26f2cedb6c151b0c60dfc12067664
Avichay Eyal <avichaye@elementor.red>
2025-07-06 11:54:41 +0300

    removing implementation of heading

--- includes/widgets/v4/heading/heading.js
+++
@@ -1,9 +0,0 @@
-initWidget( {
-	elementType: 'v4-heading',
-	callback( { element } ) {
-		const targetEl = element.querySelector( 'a' ) || element;
-		targetEl.addEventListener( 'click', function() {
-			targetEl.textContent = targetEl.textContent.split( '' ).reverse().join( '' );
-		} );
-	},
-} );
